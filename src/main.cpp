b17b48cded43c29050cc34b3034f29b1575d7a94
bouncytorch <bouncytorch@aurigrave.org>
2024-12-10 19:24:34 +0100

    Remade CMakeLists.txt to ensure I understood everything there

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "engine.h"
 #include <SDL3/SDL_main.h>
 
-int main() 
+int main(int argc, char **argv) 
 {
     Engine engine({ 1, { 1000, 500 } });
     if ( !engine.init() ) return 1;
@@ -10,7 +10,11 @@ int main()
     return 0;
 }
 
-int WinMain( int argc, char *argv[] ) 
-{
-    return main();
-}
+// int SDL_main( int argc, char *argv[] ) 
+// {
+//     Engine engine({ 1, { 1000, 500 } });
+//     if ( !engine.init() ) return 1;
+//     while ( engine.loop() );
+
+//     return 0;
+// }
